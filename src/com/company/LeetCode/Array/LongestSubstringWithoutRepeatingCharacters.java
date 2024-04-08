be1376bacfc2d96e061deb523fdfe0966f7b62d6
raokapil <40367702+raokapil@users.noreply.github.com>
2024-04-08 11:47:44 +0530

    Create LongestSubstringWithoutRepeatingCharacters.java

---
+++ src/com/company/LeetCode/Array/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,28 @@
+package com.company.LeetCode.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Using sliding window technique with a hash table.
+public class LongestSubstringWithoutRepeatingCharacters {
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> seen = new HashSet<>();
+        int i = 0, max_length = 0;
+        for (int j = 0; j < s.length(); j++) {
+            while (seen.contains(s.charAt(j))) {
+                seen.remove(s.charAt(i));
+                i++;
+            }
+            seen.add(s.charAt(j));
+            max_length = Math.max(max_length, j - i + 1);
+        }
+        return max_length;
+    }
+
+    public static void main(String[] args) {
+
+        lengthOfLongestSubstring("abc");
+    }
+}
