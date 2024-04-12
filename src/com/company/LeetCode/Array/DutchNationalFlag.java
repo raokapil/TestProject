6358afe8658fa68dad13628126b8b5f185556ba3
raokapil <40367702+raokapil@users.noreply.github.com>
2024-04-12 16:38:50 +0530

    Create DutchNationalFlag.java

---
+++ src/com/company/LeetCode/Array/DutchNationalFlag.java
@@ -0,0 +1,42 @@
+package com.company.LeetCode.String;
+
+public class DutchNationalFlag {
+
+    public static void sort(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 1, 2, 1, 0};
+        sort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
